db9fa4c65d32ecc452b3aa6419c2ab6f6ea39bb1
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-10-29 16:35:26 +0530

    Update bot_users.py

--- ForceSubscribeBot/bot_users.py
+++ ForceSubscribeBot/bot_users.py
@@ -20,4 +20,4 @@ async def users_sql(_, msg: Message):
 async def _stats(_, msg: Message):
     users = await num_users()
     chats = await num_chats()
-    await msg.reply(f"Total Users : {users} \n\nTotal Chats : {chats}", quote=True)
+    await msg.reply(f"🔎Fsub Bot Stats \n\n\n 🍁Total Users : {users} \n\n🍁Total Chats : {chats}", quote=True)
