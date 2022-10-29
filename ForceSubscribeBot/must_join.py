7d7da020f380d11e40864d13b7822b97b2097116
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-10-29 16:37:48 +0530

    Update must_join.py

--- ForceSubscribeBot/must_join.py
+++ ForceSubscribeBot/must_join.py
@@ -13,16 +13,16 @@ async def must_join_channel(bot: Client, msg: Message):
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/KatszukiNews" + MUST_JOIN
+                link = "https://t.me/EmoBotDevolopers" + MUST_JOIN
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel](t.me/KatszukiNews) to use me. After joining try again !",
+                    f"🔒You must join [this channel](t.me/{MUST_JOIN}) to use me. After joining try again !",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("🚨 Join Channel 🚨", url= t.me/KatszukiNews)]
+                        [InlineKeyboardButton("🚨 Join Channel 🚨", url= {MUST_JOIN})]
                     ])
                 )
                 await msg.stop_propagation()
