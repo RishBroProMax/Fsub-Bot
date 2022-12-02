63135d517da7d0a128e0bc66573b575d215bfe0c
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-02 13:10:11 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,7 +9,6 @@ if ENVIRONMENT:
         raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get('API_HASH', None)
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
     MUST_JOIN = os.environ.get('MUST_JOIN', None)
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN.replace("@", "")
@@ -18,7 +17,6 @@ else:
     API_ID = "14055090"
     API_HASH = "a46f7b439d0afa45b7a69fc450f754e9"
     BOT_TOKEN = "5632628980:AAEUq11V-CNJrP_KXJuZ_HkGp7UQo4EWCuo"
-    DATABASE_URL = "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     MUST_JOIN = "EmBotDevolopers"
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
