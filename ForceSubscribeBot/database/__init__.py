8b0b49d746eb80a4b9559f8b1e6e6bc8c2d7006c
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-02 13:06:36 +0530

    Update __init__.py

--- ForceSubscribeBot/database/__init__.py
+++ ForceSubscribeBot/database/__init__.py
@@ -5,7 +5,7 @@ from Config import DATABASE_URL
 
 
 def start() -> scoped_session:
-    engine = create_engine(DATABASE_URL)
+    engine = create_engine({DATABASE_URL})
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
