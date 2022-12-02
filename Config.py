5bf6783a468e2c6fa1c896747fff7ab94cce9814
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-02 11:53:16 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -17,13 +17,13 @@ if ENVIRONMENT:
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
     # Fill the Values
-    API_ID = 0
-    API_HASH = ""
-    BOT_TOKEN = ""
-    DATABASE_URL = ""
+    API_ID = "14055090"
+    API_HASH = "a46f7b439d0afa45b7a69fc450f754e9"
+    BOT_TOKEN = "5632628980:AAEUq11V-CNJrP_KXJuZ_HkGp7UQo4EWCuo"
+    DATABASE_URL = "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = "ImDark_Empire"
+    MUST_JOIN = "EmBotDevolopers"
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
 
-DEVS = [1120271521, 1875624900]
+DEVS = [5171347305,5389241071]
