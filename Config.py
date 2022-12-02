fa88ca786c605f9a226da05e05fc607ba38a96ac
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-02 13:07:15 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -10,8 +10,6 @@ if ENVIRONMENT:
     API_HASH = os.environ.get('API_HASH', None)
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
     DATABASE_URL = os.environ.get('DATABASE_URL', None)
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
-    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
     MUST_JOIN = os.environ.get('MUST_JOIN', None)
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN.replace("@", "")
@@ -21,7 +19,6 @@ else:
     API_HASH = "a46f7b439d0afa45b7a69fc450f754e9"
     BOT_TOKEN = "5632628980:AAEUq11V-CNJrP_KXJuZ_HkGp7UQo4EWCuo"
     DATABASE_URL = "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = "EmBotDevolopers"
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
