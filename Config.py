d56184b1de5eca2ede25f6d57cf9d4c50bfe8b16
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-30 17:51:11 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,28 +1,34 @@
-import os
+import Config
+import logging
+from pyrogram import Client, idle
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
-ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
 
-if ENVIRONMENT:
+logging.basicConfig(
+    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+app = Client(
+    ":memory:",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    plugins=dict(root="ForceSubscribeBot"),
+)
+
+
+# Run Bot
+if __name__ == "__main__":
     try:
-        API_ID = int(os.environ.get('API_ID', 0))
-    except ValueError:
-        raise Exception("Your API_ID is not a valid integer.")
-    API_HASH = os.environ.get('API_HASH', None)
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
-    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
-    MUST_JOIN = os.environ.get('MUST_JOIN', None)
-    if MUST_JOIN.startswith("@"):
-        MUST_JOIN = MUST_JOIN.replace("@", "")
-else:
-    # Fill the Values
-    API_ID = "14055090"
-    API_HASH = "a46f7b439d0afa45b7a69fc450f754e9"
-    BOT_TOKEN = "5632628980:AAEUq11V-CNJrP_KXJuZ_HkGp7UQo4EWCuo"
-    DATABASE_URL = "mongodb+srv://EmoAssistant :rishbro@cluster0.hoonhpy.mongodb.net/?retryWrites=true&w=majority"
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = "EmoBotDevolopers"
-    if MUST_JOIN.startswith("@"):
-        MUST_JOIN = MUST_JOIN[1:]
-
+        app.start()
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH is not valid.")
+    except AccessTokenInvalid:
+        raise Exception("Your BOT_TOKEN is not valid.")
+    uname = app.get_me().username
+    print(f"@{uname} Started Successfully!")
+    idle()
+    app.stop()
+    print("Bot stopped. Alvida!")
